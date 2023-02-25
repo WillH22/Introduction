90ec8761bd0d2911c9c0a1718eb52cf19663bea4
zefor <Williamhurtado1260@hotmail.com>
2023-02-25 01:47:26 -0500

    Working Javascript questions and functions for unit 4

---
+++ unit 4/7/app.js
@@ -0,0 +1,24 @@
+function difference(x, y) {
+  return x - y;
+}
+
+function product(x, y) {
+  return x + y;
+}
+
+function printDay(x) {
+  const days = {
+    1: "Sunday",
+    2: "Monday",
+    3: "Tuesday",
+    4: "Wednesday",
+    5: "Thursday",
+    6: "Friday",
+    7: "Saturday",
+  };
+  return days[x];
+}
+
+function lastElement(arr) {
+  return arr[arr.length - 1];
+}
