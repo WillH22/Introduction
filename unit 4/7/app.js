051c8f71d94af9a7d6daae5bf2c0699c53c038cc
zefor <Williamhurtado1260@hotmail.com>
2023-02-25 23:42:27 -0500

    Finished Unit 4, moved on to unit 7 and should be at unit 8 next

--- unit 4/7/app.js
+++ unit 4/7/app.js
@@ -22,3 +22,59 @@ function printDay(x) {
 function lastElement(arr) {
   return arr[arr.length - 1];
 }
+
+function numberCompare(x, y) {
+  if (x > y) {
+    return "First is greater";
+  } else if (x < y) {
+    return "Second is greater";
+  } else if (x === y) {
+    return "Numbers are equal";
+  }
+}
+
+function singleLetterCount(sent, l) {
+  let count = 0;
+  for (let i = 0; i < sent.length; i++) {
+    if (sent[i].toLowerCase === l.toLowerCase) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function multipleLetterCount(strg) {
+  strg = strg.toLowerCase();
+  let letters = {};
+  for (let i = 0; i < strg.length; i++) {
+    if (letters[strg[i]] === undefined) {
+      letters[strg[i]] = 1;
+    } else {
+      letters[strg[i]]++;
+    }
+  }
+  return letters;
+}
+
+function arrayManipulation(arr, command, location, value) {
+  if (command === "remove") {
+    if (location === "end") {
+      return arr.pop();
+    } else {
+      return arr.shift();
+    }
+  }
+  if (command === "add") {
+    if (location === "end") {
+      arr.push(value);
+      return arr;
+    } else {
+      arr.unshift(value);
+      return arr;
+    }
+  }
+}
+
+function isPalindrome(str) {
+  return str.toLowerCase().split("").reverse().join("") === str.toLowerCase();
+}
