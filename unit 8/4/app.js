2756b7760c917df4b2bd5d4b45724a269fcf09e2
zefor <Williamhurtado1260@hotmail.com>
2023-03-02 03:16:40 -0500

    Finished part 1 of todos list, moving on to creating a local storage for it

--- unit 8/4/app.js
+++ unit 8/4/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#todoForm");
 const todoInput = document.querySelector("#textBox");
 const ulList = document.querySelector(".Bulleted");
+const ullis = document.querySelectorAll(".Bulleted li");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -14,3 +15,34 @@ function makeTodo(text) {
   newLi.innerText = text;
   return newLi;
 }
+
+ulList.addEventListener("click", function (e) {
+  const purging = document.querySelector("#purge");
+  const crossAll = document.querySelector("#crossout");
+  const ullis = document.querySelectorAll(".Bulleted li");
+  const crullis = document.querySelectorAll(".crossed");
+
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("crossed");
+  } else if (e.target.id === "crossout") {
+    for (ulli of ullis) {
+      ulli.classList.toggle("crossed");
+    }
+  } else if (e.target.id === "purge") {
+    for (crulli of crullis) {
+      crulli.remove();
+    }
+  }
+});
+
+// The stuff below is old code before finding small bugs/mistakes with it
+
+// purging.addEventListener("click", function (e) {
+//   document.querySelector(".crossed").remove();
+// });
+// crossAll.addEventListener("click", function (e) {
+//   for (ulli of ullis) {
+//     ulli.classList.toggle("crossed");
+//   }
+// });
+// });
