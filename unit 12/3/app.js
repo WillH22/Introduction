91f4234daebaa1bf7037cdc3106444ebcc6705f3
zefor <Williamhurtado1260@hotmail.com>
2023-03-07 03:43:09 -0500

    Trying out Reduce, dislike

---
+++ unit 12/3/app.js
@@ -0,0 +1,13 @@
+function extractValue(arr, key) {
+  let newarr = arr.reduce(function (val) {
+    return;
+  });
+  return newarr;
+}
+
+const arr = [
+  { name: "Elie" },
+  { name: "Tim" },
+  { name: "Matt" },
+  { name: "Colt" },
+];
