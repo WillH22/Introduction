fce2abbefdc2021460395e9be442041c296001a4
zefor <Williamhurtado1260@hotmail.com>
2023-03-15 01:01:38 -0400

    working on python

---
+++ unit 22/2/tools.py
@@ -0,0 +1,12 @@
+def product(a, b):
+    return a * b
+
+
+"""This multiplies 2 numbers given and returns the product"""
+
+
+def weekday_name(day_of_week):
+    days = ['Sunday', 'Monday', 'Tuesday',
+            'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+    return days.index(day_of_week)
