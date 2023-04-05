494166ce2e95b46d41f79a020be358de6233b9ce
zefor <Williamhurtado1260@hotmail.com>
2023-04-05 16:48:49 -0400

    Trying to figure out local storage

--- unit 8/4/app.js
+++ unit 8/4/app.js
@@ -1,3 +1,50 @@
+// const form = document.querySelector("#todoForm");
+// const todoInput = document.querySelector("#textBox");
+// const ulList = document.querySelector(".Bulleted");
+// const ullis = document.querySelectorAll(".Bulleted li");
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const newTodo = makeTodo(todoInput.value);
+//   ulList.append(newTodo);
+//   todoInput.value = "";
+// });
+
+// function makeTodo(text) {
+//   const newLi = document.createElement("li");
+//   newLi.innerText = text;
+//   return newLi;
+// }
+
+// ulList.addEventListener("click", function (e) {
+//   const ullis = document.querySelectorAll(".Bulleted li");
+//   const crullis = document.querySelectorAll(".crossed");
+
+//   if (e.target.tagName === "LI") {
+//     e.target.classList.toggle("crossed");
+//   } else if (e.target.id === "crossout") {
+//     for (ulli of ullis) {
+//       ulli.classList.toggle("crossed");
+//     }
+//   } else if (e.target.id === "purge") {
+//     for (crulli of crullis) {
+//       crulli.remove();
+//     }
+//   }
+// });
+
+// The stuff below is old code before finding small bugs/mistakes with it
+
+// purging.addEventListener("click", function (e) {
+//   document.querySelector(".crossed").remove();
+// });
+// crossAll.addEventListener("click", function (e) {
+//   for (ulli of ullis) {
+//     ulli.classList.toggle("crossed");
+//   }
+// });
+// });
+
 const form = document.querySelector("#todoForm");
 const todoInput = document.querySelector("#textBox");
 const ulList = document.querySelector(".Bulleted");
@@ -6,7 +53,10 @@ const ullis = document.querySelectorAll(".Bulleted li");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const newTodo = makeTodo(todoInput.value);
-  ulList.append(newTodo);
+  localStorage.setItem("list", newTodo.value);
+  const newestTodo = "";
+  newestTodo.innerText = localStorage.getItem("list");
+  ulList.append(newestTodo);
   todoInput.value = "";
 });
 
@@ -17,8 +67,6 @@ function makeTodo(text) {
 }
 
 ulList.addEventListener("click", function (e) {
-  const purging = document.querySelector("#purge");
-  const crossAll = document.querySelector("#crossout");
   const ullis = document.querySelectorAll(".Bulleted li");
   const crullis = document.querySelectorAll(".crossed");
 
@@ -34,15 +82,3 @@ ulList.addEventListener("click", function (e) {
     }
   }
 });
-
-// The stuff below is old code before finding small bugs/mistakes with it
-
-// purging.addEventListener("click", function (e) {
-//   document.querySelector(".crossed").remove();
-// });
-// crossAll.addEventListener("click", function (e) {
-//   for (ulli of ullis) {
-//     ulli.classList.toggle("crossed");
-//   }
-// });
-// });
