b45449604e1b925ca8cf17e96dcbc0127592e99a
zefor <Williamhurtado1260@hotmail.com>
2023-04-14 00:24:43 -0400

    Finished Todo list, added localstorage and a new button 'uncrossout'

--- unit 8/4/app.js
+++ unit 8/4/app.js
@@ -1,3 +1,5 @@
+"Toda list without local storage";
+
 // const form = document.querySelector("#todoForm");
 // const todoInput = document.querySelector("#textBox");
 // const ulList = document.querySelector(".Bulleted");
@@ -44,21 +46,48 @@
 //   }
 // });
 // });
+"My Todo list with localstorage";
 
 const form = document.querySelector("#todoForm");
 const todoInput = document.querySelector("#textBox");
 const ulList = document.querySelector(".Bulleted");
 const ullis = document.querySelectorAll(".Bulleted li");
+const purgebutton = document.querySelector("#purge");
+const listSection = document.querySelector("#results");
+let todoArray = [];
+
+window.addEventListener("load", () => {
+  displaylist();
+});
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const newTodo = makeTodo(todoInput.value);
-  localStorage.setItem("list", newTodo.value);
-  const newestTodo = "";
-  newestTodo.innerText = localStorage.getItem("list");
-  ulList.append(newestTodo);
+  let todo = localStorage.getItem("todo");
+  if (todo === null) {
+    todoArray = [];
+  } else {
+    todoArray = JSON.parse(todo);
+    console.log(todoArray);
+  }
+  todoArray.push(todoInput.value);
+  localStorage.setItem("todo", JSON.stringify(todoArray));
   todoInput.value = "";
+  displaylist();
 });
+
+function displaylist() {
+  let todo = localStorage.getItem("todo");
+  if (todo === null) {
+    todoArray = [];
+  } else {
+    todoArray = JSON.parse(todo);
+  }
+  ulList.innerHTML = "";
+  todoArray.map((item) => {
+    let newTodo = makeTodo(item);
+    ulList.append(newTodo);
+  });
+}
 
 function makeTodo(text) {
   const newLi = document.createElement("li");
@@ -66,19 +95,31 @@ function makeTodo(text) {
   return newLi;
 }
 
-ulList.addEventListener("click", function (e) {
+listSection.addEventListener("click", function (e) {
+  "The commented out areas were just me testing out what would work and what won't, I deleted some of it not gonna lie, but the console.log was helping, randomArr helped figure out why the text wasn't showing up and I realize it needs to be innerText";
+
   const ullis = document.querySelectorAll(".Bulleted li");
   const crullis = document.querySelectorAll(".crossed");
+  // randomArr = [];
 
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("crossed");
   } else if (e.target.id === "crossout") {
     for (ulli of ullis) {
-      ulli.classList.toggle("crossed");
+      ulli.classList = "crossed";
+    }
+  } else if (e.target.id === "uncross") {
+    for (ulli of ullis) {
+      ulli.classList = "";
     }
   } else if (e.target.id === "purge") {
     for (crulli of crullis) {
-      crulli.remove();
+      todoArray.splice(todoArray.indexOf(crulli.innerText), 1);
+      // todoArray.splice(crulli, 1);
+      localStorage.setItem("todo", JSON.stringify(todoArray));
+      displaylist();
+      // randomArr.push(crulli.innerText);
     }
+    // console.log(todoArray);
   }
 });
