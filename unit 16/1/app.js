3ab0a2e5f022582a1dace3469cc916143e9d54f4
zefor <Williamhurtado1260@hotmail.com>
2023-04-16 21:34:31 -0400

    Finished 16.1

---
+++ unit 16/1/app.js
@@ -0,0 +1,49 @@
+class Vehicle {
+  constructor(a, b, c) {
+    this.make = a;
+    this.model = b;
+    this.year = c;
+  }
+  honk() {
+    console.log("Beep");
+  }
+  toString() {
+    console.log(
+      `The vehicle is a ${this.make} ${this.model} from ${this.year}`
+    );
+  }
+}
+
+class Car extends Vehicle {
+  constructor(a, b, c) {
+    super(a, b, c);
+  }
+  numWheels = 4;
+}
+
+class Motorcycle extends Vehicle {
+  constructor(a, b, c) {
+    super(a, b, c);
+  }
+  numWheels = 2;
+  revEngine() {
+    console.log("VROOM!!!");
+  }
+}
+
+class Garage {
+  constructor(a) {
+    this.cap = a;
+    this.vehicles = [];
+  }
+  add(newcar) {
+    if (!(newcar instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if (this.vehicles >= this.cap) {
+      return "Sorry we're full.";
+    }
+    this.vehicles.push(newcar);
+    return "Vehicle added!";
+  }
+}
